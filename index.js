1da70334259bef4d093b37227cf9de58e1d14a6e
kiko <wfmellado@gmail.com>
2021-04-08 17:07:29 -0300

    teste se repo e xhelpers

--- index.js
+++ index.js
@@ -7,14 +7,39 @@ const inquirer = require("./lib/command");
 
 clear();
 
+console.log(chalk.green(figlet.textSync("xcli", { horizontalLayout: "full" })));
+
 if (files.directoryExists(".git")) {
-  console.log(chalk.red("Already a Git repository!"));
-  process.exit();
+  console.log(
+    chalk.yellow("Already a Git repository! - Hope it uses xhelpers-api ;)")
+  );
+  // process.exit();
+}
+console.log(chalk.blue(`Running folder: ${files.getCurrentDirectoryBase()}`));
+
+let errFolders = false;
+
+if (!files.directoryExists("model")) {
+  console.log(chalk.yellow("'model' folder not found"));
+  errFolders = true;
+}
+if (!files.directoryExists("routes")) {
+  console.log(chalk.yellow("'routes' folder not found"));
+  errFolders = true;
+}
+if (!files.directoryExists("services")) {
+  console.log(chalk.yellow("'services' folder not found"));
+  errFolders = true;
 }
 
-console.log(
-  chalk.yellow(figlet.textSync("xcli", { horizontalLayout: "full" }))
-);
+if (errFolders) {
+  console.log(
+    chalk.red(
+      "Required folders not found maybe its not an xhelpers-api project"
+    )
+  );
+  process.exit();
+}
 
 const run = async () => {
   const projectType = await inquirer.askXhelpersType();
